3eb1786f68b260f88f3888f061ec70f251b965f1
Mamadou Alpha Diallo <3010madiallo@student.epfc.eu>
2025-10-02 18:36:10 +0200

    premier commit pour la revision java

---
+++ Dev1TD6_Git/src/HelloGit.java
@@ -0,0 +1,8 @@
+public class HelloGit {
+    static void main( String [] args ) {
+
+        System.out.println("Alpha diallo on JAVA");
+
+        System.out.println("Alpha diallo on GIT");
+    }
+}
