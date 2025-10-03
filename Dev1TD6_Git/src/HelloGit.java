87496f9b9e201726ed22172e10c92213ffe97d47
Mamadou Alpha Diallo <3010madiallo@student.epfc.eu>
2025-10-03 10:58:10 +0200

    reprise java

--- Dev1TD6_Git/src/HelloGit.java
+++ Dev1TD6_Git/src/HelloGit.java
@@ -4,5 +4,11 @@ public class HelloGit {
         System.out.println("Alpha diallo on JAVA");
 
         System.out.println("Alpha diallo on GIT");
+        System.Out.println(somme(5+3));
+    
+    }
+
+    public static int somme(int a ,int b){
+        return a+b;
     }
 }
