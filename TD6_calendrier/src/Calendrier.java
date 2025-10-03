6f831ebd69bf95c270a2974edde313a233459d6d
Mamadou Alpha Diallo <3010madiallo@student.epfc.eu>
2025-10-03 12:32:20 +0200

    exo4 en cours

--- TD6_calendrier/src/Calendrier.java
+++ TD6_calendrier/src/Calendrier.java
@@ -1,4 +1,17 @@
+import java.util.Scanner;
 public class Calendrier {
+
+    public  static void main( String[] args ) {
+
+        Scanner clavier = new Scanner(System.in);
+        System.out.println("Veillez entrer le moi en entier");
+        int mois=clavier.nextInt();
+        System.out.println("Veillez entrer l'anné en entier");
+        int annee=clavier.nextInt();
+        afficherTitre(mois,annee);
+        afficherEntête();
+
+    }
     public static String nomMois( int mois){
         String [] tab= new String [12];
         tab[0] = "Janvier";
@@ -13,13 +26,22 @@ public class Calendrier {
         tab[9] = "Octobre";
         tab[10] = "Novembre";
 
-        return tab[mois];
+        return tab[mois-1];
     }
 
 
 
 
     public static void afficherTitre(int mois, int année){
+
+
+        System.out.println("=======================");
+        System.out.println("     "+nomMois(mois)+" "+année);
+        System.out.println("=======================");
+    }
+
+  static void AfficherEntête
+        System.out.println("LU  Ma  Me  Je  Ve  Sa  Di");
 
     }
 
