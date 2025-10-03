e53d02ed03e617053e9affd6d298c3b4115e4217
Mamadou Alpha Diallo <3010madiallo@student.epfc.eu>
2025-10-03 11:51:17 +0200

    working on calender

---
+++ TD6_calendrier/src/Calendrier.java
@@ -0,0 +1,26 @@
+public class Calendrier {
+    public static String nomMois( int mois){
+        String [] tab= new String [12];
+        tab[0] = "Janvier";
+        tab[1] = "Février";
+        tab[2] = "Mars";
+        tab[3] = "Avril";
+        tab[4] = "Mai";
+        tab[5] = "Juin";
+        tab[6] = "Juillet";
+        tab[7] = "Aout";
+        tab[8] = "Septembre";
+        tab[9] = "Octobre";
+        tab[10] = "Novembre";
+
+        return tab[mois];
+    }
+
+
+
+
+    public static void afficherTitre(int mois, int année){
+
+    }
+
+}
