5e0f3d52e918b617dff5cdd922afc6bf8337bc17
Mamadou Alpha Diallo <3010madiallo@student.epfc.eu>
2025-10-07 08:38:27 +0200

    exo7

--- TD6_calendrier/src/Calendrier.java
+++ TD6_calendrier/src/Calendrier.java
@@ -1,19 +1,22 @@
 import java.util.Scanner;
+
 public class Calendrier {
 
-    public  static void main( String[] args ) {
-/**
- Scanner clavier = new Scanner(System.in);
- System.out.println("Veillez entrer le moi en entier");
- int mois=clavier.nextInt();
- System.out.println("Veillez entrer l'anné en entier");
- int annee=clavier.nextInt();
- afficherTitre(mois,annee);*/
+    public static void main(String[] args) {
+        /**
+         * Scanner clavier = new Scanner(System.in);
+         * System.out.println("Veillez entrer le moi en entier");
+         * int mois=clavier.nextInt();
+         * System.out.println("Veillez entrer l'anné en entier");
+         * int annee=clavier.nextInt();
+         * afficherTitre(mois,annee);
+         */
         AfficherEntête();
-        AfficherMois(1,28);
+        AfficherMois(1, 28);
     }
-    public static String nomMois( int mois){
-        String [] tab= new String [12];
+
+    public static String nomMois(int mois) {
+        String[] tab = new String[12];
         tab[0] = "Janvier";
         tab[1] = "Février";
         tab[2] = "Mars";
@@ -27,49 +30,46 @@ public class Calendrier {
         tab[10] = "Novembre";
         tab[11] = "Decembre";
 
-        return tab[mois-1];
+        return tab[mois - 1];
     }
 
-
-    public static void afficherTitre(int mois, int année){
-
+    public static void afficherTitre(int mois, int année) {
 
         System.out.println("=======================");
-        System.out.println("     "+nomMois(mois)+" "+année);
+        System.out.println("     " + nomMois(mois) + " " + année);
         System.out.println("=======================");
     }
 
-    public static void AfficherEntête(){
+    public static void AfficherEntête() {
         System.out.println("LU  Ma  Me  Je  Ve  Sa  Di");
 
     }
 
-    public static void AfficherMois(int decalage, int nombreDeJour){
+    public static void AfficherMois(int decalage, int nombreDeJour) {
 
-        int [][]tab=  new int[5][7];
+        int[][] tab = new int[5][7];
 
-        int ligne=0;
-        int jour=1;
-        int col=0;
+        int ligne = 0;
+        int jour = 1;
+        int col = 0;
         for (int i = 0; i < decalage; i++) {
             System.out.print("    "); // 4 espaces pour une case vide
         }
-        while(ligne<tab.length){
+        while (ligne < tab.length) {
 
-            if(ligne==0){
-                col=col+decalage;
+            if (ligne == 0) {
+                col = col + decalage;
+            } else {
+                col = 0;
+
             }
-            else{ col=0;
+            while (col < tab[0].length && jour < nombreDeJour + 1) {
+                tab[ligne][col] = jour;
 
-            }
-            while(col<tab[0].length && jour<nombreDeJour+1){
-                tab[ligne][col]=jour;
-
-                if(jour<10){
-                    System.out.print( "0"+tab[ligne][col]+"  ");
-                }
-                else{
-                    System.out.print( tab[ligne][col]+"  ");
+                if (jour < 10) {
+                    System.out.print("0" + tab[ligne][col] + "  ");
+                } else {
+                    System.out.print(tab[ligne][col] + "  ");
 
                 }
 
@@ -82,5 +82,15 @@ public class Calendrier {
         }
     }
 
+    public boolean estBixestile(int annee) {
+        if (annee % 400 == 0) {
+            return true;
+        }
+
+        if (annee % 4 == 0 && annee % 100 != 0) {
+            return true;
+        }
+        return false;
+    }
 
 }
