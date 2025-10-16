8ed21fdd355f667b46076d587f1867b50ff1a551
Mamadou Alpha Diallo <3010madiallo@student.epfc.eu>
2025-10-16 21:25:29 +0200

    palindrome fait

--- Test/src/main/java/TD10_TEST/Exercices.java
+++ Test/src/main/java/TD10_TEST/Exercices.java
@@ -10,12 +10,22 @@ package TD10_TEST;
  */
 public class Exercices {
 
-    public boolean Palindrom(String mot) {
+    public static void main(String[] args) {
+        System.out.println(Palindrom("levela"));
+    }
+
+    public static boolean Palindrom(String mot) {
 
         int i = 0;
-        int j=mot.length()-1;
+        int j = mot.length() - 1;
         while (i < mot.length() / 2) {
-            if mot[i]!=mot
+            if (mot.charAt(i) != mot.charAt(j)) {
+                return false;
+
+            }
+            i++;
+            j--;
         }
+        return true;
     }
 }
