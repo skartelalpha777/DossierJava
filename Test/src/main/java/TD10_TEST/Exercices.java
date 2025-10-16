2b6f8821898a6c1b99014eea8b3bcd4f94e33055
Mamadou Alpha Diallo <3010madiallo@student.epfc.eu>
2025-10-16 21:07:49 +0200

    reprise avec les test

---
+++ Test/src/main/java/TD10_TEST/Exercices.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TD10_TEST;
+
+/**
+ *
+ * @author diall
+ */
+public class Exercices {
+
+    public boolean Palindrom(String mot) {
+
+        int i = 0;
+        int j=mot.length()-1;
+        while (i < mot.length() / 2) {
+            if mot[i]!=mot
+        }
+    }
+}
